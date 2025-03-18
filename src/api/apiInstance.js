b5c1afae3a42a6cf461d55ba3bf65213d6c3875c
Babloo Kumar <97720510+Babloo321@users.noreply.github.com>
2025-03-19 00:10:49 +0530

    Update apiInstance.js

--- src/api/apiInstance.js
+++ src/api/apiInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const BASE_URL = "https://watch-backend-two.vercel.app/api/v1/";
-// const BASE_URL = "http://localhost:5050/api/v1/";
+// const BASE_URL = "http://localhost:5050/api/v1";
 const API = axios.create({
   baseURL: BASE_URL
 });
